2847b5a4ac0317d0fa753c779667f457fcf02879
pujitha <siripujitha9701@gamil.com>
2022-02-09 00:09:28 +0530

    REACT-JS

---
+++ src/FirstReact.js
@@ -0,0 +1,14 @@
+import img from "./image.jpg";
+function FirstReact(){
+    return(
+        <>
+ <center><img src={img} alt=""/><br></br><h1>
+   NATURE</h1><h3>"Nature" refers to the phenomena of the physical world, and also to life in general.<br></br>
+    It ranges in scale from the subatomic to the cosmic. The term "nature" refer to 
+     <br></br>living plants and animals,geological processes, weather,physics, such as 
+     matter and energy.</h3>
+     <a href="https://images.news18.com/ibnlive/uploads/2021/07/1627377451_nature-1600x900.jpg">SHARE</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='https://www.nature.com/'>Learn More</a>
+     <br></br><br></br></center></>
+    )
+}
+export default FirstReact;
