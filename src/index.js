e1ddf538bcfe65feb58524deb7f10d0817f81bf4
pujitha <siripujitha9701@gamil.com>
2022-02-08 23:57:51 +0530

    REACT-JS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import img from "./image.jpg";
+import "./styel.css";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <>
+ <center><img src={img} alt=""/><br></br><h1>
+   NATURE</h1><h3>"Nature" refers to the phenomena of the physical world, and also to life in general.<br></br>
+    It ranges in scale from the subatomic to the cosmic. The term "nature" refer to 
+     <br></br>living plants and animals,geological processes, weather,physics, such as 
+     matter and energy.</h3>
+     <a href="https://images.news18.com/ibnlive/uploads/2021/07/1627377451_nature-1600x900.jpg">SHARE</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='https://www.nature.com/'>Learn More</a>
+     <br></br><br></br></center></>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
